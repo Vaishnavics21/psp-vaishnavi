64b393e0a44d74ecbb2399aa959a6b0887076819
Vaishnavics21 <101888261+Vaishnavics21@users.noreply.github.com>
2022-03-21 19:02:16 +0530

    Create additionoftwonumbers.c

---
+++ additionoftwonumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int a,b,c;
+  clrscr();
+  printf("enter values for a and b\n");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("The sum is;%d",c);
+  getch();
+  return 0;
+}
